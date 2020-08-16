b9abd90c7bdb7c11ca585f8593bca59713d60880
msertacc <m_sertac.yilmaz@hotmail.com>
2020-08-16 05:54:19 +0300

    İkinci Güncelleme
    Bu güncellemede veritabanı eklendi. Authorization işleminin ilk aşaması eklendi.

---
+++ WebProje/WebProje/WebProje/WebProje/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WebProje.Models.EntityFramework;
+
+namespace WebProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        VeritabanıEntities1 db = new VeritabanıEntities1();
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("Editor")]
+        public ActionResult Editor()
+        {
+            return View();
+        }
+        [Route("Kategoriler")]
+        public ActionResult Kategoriler()
+        {
+            var veriler = db.FILMLER.ToList();
+
+            return View(veriler);
+        }
+        [Route("Giris")]
+        public ActionResult Giris()
+        {
+            return View();
+        }
+    }
+}
